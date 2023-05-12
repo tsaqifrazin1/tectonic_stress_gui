1b44a387f1264528c63d3c6ee40b9b6f4cd082bb
Tsaqif Razin <72601006+tsaqifrazin1@users.noreply.github.com>
2023-05-12 16:22:54 +0800

    update for time series

--- main.py
+++ main.py
@@ -1,184 +1,377 @@
+import tempfile
 import tkinter as tk
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+from matplotlib.backend_bases import NavigationToolbar2
+import pandas as pd
 from rumus import *
 import pygmt
-import pandas as pd
 from PIL import Image, ImageTk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+from window_stress_time_function import *
 
 
-# Create the main window
-root = tk.Tk()
-root.title("Calculate Tectonic Stress")
+def open_file():
+    """
+    Open a file for editing.
 
-root.geometry("800x800")
-root.resizable(False,False)
+    Parameters:
+    None
 
-# Disable automatic resizing of widgets
-root.grid_propagate(False)
+    Returns:
+    None
+    """
+    global filepath
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.dat")]
+    )
+    if not filepath:
+        return
+    text = pd.read_csv(filepath, sep='\s+', header=None, skiprows=1)
+    if check_file_format(text) != True:
+        filepath = None
+    messagebox.showinfo("File Opened", "File opened successfully")
 
-# Configure grid rows and columns to expand and fill the space available
-for i in range(10):
-    root.grid_rowconfigure(i, weight=1)
-for i in range(2):
-    root.grid_columnconfigure(i, weight=1)
-
-frame = tk.LabelFrame(root, text="Input Strain", padx=10, pady=10)
-frame.pack(padx=1, pady=1, side='left', anchor='n')
-
-# Create the labels for each variable
-label1 = tk.Label(frame, text="strain_xx (nanostrain)")
-label1.grid(row=0, column=0, padx=5, pady=5, sticky='w')
-
-label2 = tk.Label(frame, text="strain_yy (nanostrain)")
-label2.grid(row=1, column=0, padx=5, pady=5, sticky='w')
-
-label3 = tk.Label(frame, text="strain_xy (nanostrain)")
-label3.grid(row=2, column=0, padx=5, pady=5, sticky='w')
-
-# Create the input fields for each variable
-entry1 = tk.Entry(frame)
-entry1.grid(row=0, column=1, padx=5, pady=5)
-
-entry2 = tk.Entry(frame)
-entry2.grid(row=1, column=1, padx=5, pady=5)
-
-entry3 = tk.Entry(frame)
-entry3.grid(row=2, column=1, padx=5, pady=5)
-
-frame2 = tk.LabelFrame(root,padx=10, pady=10, text="Input Additional Variables")
-frame2.pack(padx=1, pady=1, side='top', anchor='n')
-# Create input for E,v,miu
-E = tk.StringVar()
-E.set("80000000")
-v = tk.StringVar()
-v.set("0.25")
-miu = tk.StringVar()
-miu.set("0.4")
-
-# Create the labels for the additional variables
-label4 = tk.Label(frame2, text="Modulus Young (kPa)")
-label4.grid(row=0, column=2, padx=5, pady=5, sticky='w')
-
-label5 = tk.Label(frame2, text="Poisson ratio")
-label5.grid(row=1, column=2, padx=5, pady=5, sticky='w')
-
-label6 = tk.Label(frame2, text="Fric. Coefficient")
-label6.grid(row=2, column=2, padx=5, pady=5, sticky='w')
-
-# Create the input fields for the additional variables
-entry4 = tk.Entry(frame2, textvariable=E)
-entry4.grid(row=0, column=3, padx=5, pady=5)
-
-entry5 = tk.Entry(frame2, textvariable=v)
-entry5.grid(row=1, column=3, padx=5, pady=5)
-
-entry6 = tk.Entry(frame2, textvariable=miu)
-entry6.grid(row=2, column=3, padx=5, pady=5)
-
-# Create input for long, lat
-long = tk.StringVar()
-long.set('120.9')
-lat = tk.StringVar()
-lat.set('-1.75')
+    # TODO: use function to check the file format
 
 
-frame3 = tk.LabelFrame(root,padx=10, pady=10, text="Input Strain Coordinate")
-frame3.pack(padx=14, pady=1, side='top', anchor='n', expand=True, fill='x')
-# Create the labels for the additional variables
-label7 = tk.Label(frame3, text="Longitude")
-label7.grid(row=0, column=2, padx=5, pady=5, sticky='w',)
+# TODO: Add a function to checking the file format when open file
+def check_file_format(text):
+    """
+    Check the file format
 
-label8 = tk.Label(frame3, text="Latitude")
-label8.grid(row=1, column=2, padx=5, pady=5, sticky='w')
+    Example:
+    strain_xx strain_yy strain_xy long lat
+    -66.7 88.4 -15.1 120.9 -1.75
 
-# Create the input fields for the additional variables
-entry7 = tk.Entry(frame3, textvariable=long)
-entry7.grid(row=0, column=3, padx=5, pady=5)
+    Parameters:
+    text (pandas dataframe): the data from the file
 
-entry8 = tk.Entry(frame3, textvariable=lat)
-entry8.grid(row=1, column=3, padx=5, pady=5)
+    Returns:
+    bool: True if the file format is correct, False if the file format is incorrect
+    """
+    # check the number of columns
+    if text.shape[1] != 5:
+        messagebox.showerror('File Format Incorrect',
+                             'The number of columns is incorrect')
+        return False
+    # check all the strain_xx
+    for i in range(0, len(text[0])):
+        if text[0][i] < -1000 or text[0][i] > 1000:
+            messagebox.showerror(
+                'File Format Incorrect', 'The strain_xx value is out of range (-1000, 1000) in line {}'.format(i+2))
+            return False
+    # check all the strain_yy
+    for i in range(0, len(text[1])):
+        if text[1][i] < -1000 or text[1][i] > 1000:
+            messagebox.showerror(
+                'File Format Incorrect', 'The strain_yy value is out of range (-1000, 1000) in line {}'.format(i+2))
+            return False
+    # check all the strain_xy
+    for i in range(0, len(text[2])):
+        if text[2][i] < -1000 or text[2][i] > 1000:
+            messagebox.showerror(
+                'File Format Incorrect', 'The strain_xy value is out of range (-1000, 1000) in line {}'.format(i+2))
+            return False
+    # check all the long
+    for i in range(0, len(text[3])):
+        if text[3][i] < -180 or text[3][i] > 180:
+            messagebox.showerror(
+                'File Format Incorrect', 'The long value is out of range (-180, 180) in line {}'.format(i+2))
+            return False
+    # check all the lat
+    for i in range(0, len(text[4])):
+        if text[4][i] < -90 or text[4][i] > 90:
+            messagebox.showerror(
+                'File Format Incorrect', 'The lat value is out of range (-90, 90) in line {}'.format(i+2))
+            return False
+    return True
 
 
-# Function to retrieve the input values
-def get_values():
+def calculate_tectonic_stress():
+    """
+    Calculate the tectonic stress
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    global E
+    global v
+    global miu
+    global long_min
+    global long_max
+    global lat_min
+    global lat_max
     global display
-    strain_xx = float(entry1.get())*1e-9
-    strain_yy = float(entry2.get())*1e-9
-    strain_xy = float(entry3.get())*1e-9
-    E = float(entry4.get())
-    v = float(entry5.get())
-    miu = float(entry6.get())
-    long = float(entry7.get())
-    lat = float(entry8.get())
+    global list_cfs
+    if 'filepath' not in globals() or filepath == None:
+        messagebox.showerror('No File Opened', 'Please open a file first')
+        return
 
-    # Calculate Tectonic Stress
-    stress_tensor = stress_from_strain_skripsi(strain_xx, strain_yy, strain_xy, E, v)
+    E = float(inp_E.get())
+    v = float(inp_v.get())
+    miu = float(inp_miu.get())
+    long_min = float(inp_long_min.get())
+    long_max = float(inp_long_max.get())
+    lat_min = float(inp_lat_min.get())
+    lat_max = float(inp_lat_max.get())
 
-    w, v = principal_stress_from_eig(stress_tensor[0][0], stress_tensor[1][0], stress_tensor[2][0])
+    text = pd.read_csv(filepath, sep='\s+', header=None, skiprows=1)
 
-    theta = np.degrees((np.arctan(2*stress_tensor[2][0]/(stress_tensor[0][0] - stress_tensor[1][0])))/2)
-
-    degrees = np.arctan(v[1][0]/v[0][0])
-    azimuth = abs(np.degrees(degrees)) - 180
-
-    sigmax = w[1]
-    sigmay = w[0]
-
-    lon_min = long - 1
-    lon_max = long + 1
-    lat_min = lat - 1
-    lat_max = lat + 1
-
-    normal, strain = normal_and_shear_from_pdf(sigmax, sigmay, stress_tensor[2][0], theta)
-
-    cfs = abs(strain) + miu * normal
-
-    # Remove existing labels
-    for widget in root.grid_slaves():
-        if int(widget.grid_info()["row"]) > 3:
-            widget.destroy()
-    
+    # Calculate the tectonic stress and save it to a file
+    list_cfs = []
     with open('output.dat', 'w') as f:
-        f.writelines("{}  {}  {}  {}  {}".format(str(long), str(lat+0.15), str(w[0]/100), str(w[1]/100), str(azimuth)))
+        for i in range(0, len(text[0])):
+            stress_tensor = stress_from_strain_skripsi(
+                text[0][i]*1e-9, text[1][i]*1e-9, text[2][i]*1e-9, E, v)
+            w, vec = principal_stress_from_eig(
+                stress_tensor[0][0], stress_tensor[1][0], stress_tensor[2][0])
+            degrees = np.arctan(vec[1][0]/vec[0][0])
+            azimuth = abs(np.degrees(degrees)) - 180
+            theta = np.degrees(
+                (np.arctan(2*stress_tensor[2][0]/(stress_tensor[0][0] - stress_tensor[1][0])))/2)
 
-    # Create labels to display the submitted values in the grid
-    tk.Label(tectonic_stress_result, text="Tectonic Stress (kPa/tahun): ").grid(row=1, column=0, padx=1, pady=5, sticky='w')
-    tk.Label(tectonic_stress_result, text='{:.2f}'.format(cfs)).grid(row=1, column=1, padx=5, pady=5, sticky='w')
+            if(w[0] > w[1]):
+                sigmax = w[0]
+                sigmay = w[1]
+            else:
+                sigmax = w[1]
+                sigmay = w[0]
+
+            normal, strain = normal_and_shear_from_pdf(
+                sigmax, sigmay, stress_tensor[2][0], theta)
+
+            cfs = abs(strain) + miu * normal
+            list_cfs.append(cfs)
+
+            f.writelines("{}  {}  {}  {}  {}  {}\n".format(str(text[3][i]), str(
+                text[4][i]), str(w[0]/100), str(w[1]/100), str(azimuth), cfs))
 
     data = pd.read_csv('output.dat', sep='\s+', header=None)
-    print(data)
+    data_used = data.iloc[:, 0:5].copy(deep=True)
     fig = pygmt.Figure()
-    region = [lon_min, lon_max, lat_min, lat_max]
+    region = [long_min, long_max, lat_min, lat_max]
     proj = 'M' + str(12) + 'c'
     fig.basemap(region=region, projection=proj, frame='a0.5')
-    fig.coast(region=region, projection=proj, land='white', shorelines="1/0.5p")
-    fig.velo(
-        data=data,
-        spec='x10',
-        pen='0.5p', 
-        color='black',
-    )
-    fig.plot(x=long, y=lat, style='c0.2c', color='red', pen='0.5p')
+    fig.coast(region=region, projection=proj,
+              land='white', shorelines="1/0.5p")
     fig.plot(
         data='indonesiafaults.gmt',
     )
-    fig.savefig('azimuth.png')
+    for i in range(len(text[0])):
+        fig.plot(x=data[0][i], y=data[1][i], style='c0.2c', fill='red')
+        data_used[1][i] = data_used[1][i] + 0.07
+        fig.velo(
+            data=data_used.iloc[[i]],
+            spec='x10',
+            pen='0.5p',
+            fill='black',
+        )
+        fig.text(x=data[0][i], y=data[1][i]-0.02,
+                 text='CFS: {:.2f}'.format(data[5][i]), font='8p,Helvetica-Bold,black')
+        fig.text(x=data[0][i], y=data[1][i]-0.05,
+                 text=i, font='8p,Helvetica-Bold,red')
+    fig.savefig('azimuth.png', dpi=300)
+
+    frm_time_series = find_canvas_by_text(root, 'Time Series')
+    frm_time_series.destroy() if frm_time_series else None
+
+    root.resizable(True, True)
+    root.geometry("785x480")
+    root.resizable(False, False)
+
     image = Image.open('azimuth.png')
-    image.thumbnail((474,447))
+    image.thumbnail((474,447), Image.LANCZOS, reducing_gap=5.0)
     display = ImageTk.PhotoImage(image)
-    image = tk.Label(frame4, image=display, background='white', width=474, height=447, padx=10, pady=10)
-    image.grid(row=0, column=0, padx=5, pady=5)
+    frm_image = tk.LabelFrame(root, borderwidth=0, highlightthickness=0,
+                              text='Azimuth', labelanchor='n', font=('Helvetica', 12, 'bold'))
+    frm_image.place(width=785, height=480)
+    image = tk.Label(frm_image, image=display, background='white', width=474, height=447, padx=10, pady=10)
+    image.place(anchor='center', relx=0.5, rely=0.5)
 
 
-# Create the button to submit the values
-tectonic_stress_result = tk.LabelFrame(root, text="Tectonic Stress Result", padx=15, pady=15)
-tectonic_stress_result.place(anchor='w', rely=0.27, relx=0.001,  width=387, height=120)
-submit_button = tk.Button(tectonic_stress_result, text="Submit", command=get_values)
-submit_button.grid(row=0, column=0, padx=5, pady=5, sticky='e')
+def window_tectonic_stress():
+    """
+    Calculate the tectonic stress
 
-frame4 = tk.LabelFrame(root,padx=145, text="Azimuth")
-frame4.place(anchor='w', rely=0.65, relx=0.001, width=785, height=480)
-image = tk.Label(frame4)
-image.grid(row=0, column=0)
+    Parameters:
+    None
 
-# Start the main loop
+    Returns:
+    None
+    """
+    global inp_E
+    global inp_v
+    global inp_miu
+    global inp_long_min
+    global inp_long_max
+    global inp_lat_min
+    global inp_lat_max
+    if 'filepath' not in globals() or filepath == None:
+        messagebox.showerror('No File Opened', 'Please open a file first')
+        return
+
+    global wdw_tectonic_stress
+    wdw_tectonic_stress = tk.Toplevel(root)
+    # Create the button to calculate the tectonic stress
+    btn_tectonic_stress_calculate = tk.Button(
+        wdw_tectonic_stress, text="Calculate", command=run_tectonic_stress)
+    btn_tectonic_stress_calculate.grid(row=8, column=2, padx=5, pady=5)
+
+    # Create the button to close the window
+    btn_tectonic_stress_close = tk.Button(
+        wdw_tectonic_stress, text="Close", command=wdw_tectonic_stress.destroy)
+    btn_tectonic_stress_close.grid(row=8, column=3, padx=5, pady=5)
+
+    # Create input for E,v,miu
+    E = tk.StringVar()
+    E.set("80000000")
+    v = tk.StringVar()
+    v.set("0.25")
+    miu = tk.StringVar()
+    miu.set("0.4")
+    long_min = tk.StringVar()
+    long_min.set("120")
+    long_max = tk.StringVar()
+    long_max.set("121")
+    lat_min = tk.StringVar()
+    lat_min.set("-2.5")
+    lat_max = tk.StringVar()
+    lat_max.set("-1")
+
+    # Create the labels for the additional variables
+    lbl_E = tk.Label(wdw_tectonic_stress, text="Modulus Young (kPa)")
+    lbl_E.grid(row=0, column=2, padx=5, pady=5, sticky='w')
+
+    lbl_v = tk.Label(wdw_tectonic_stress, text="Poisson ratio")
+    lbl_v.grid(row=1, column=2, padx=5, pady=5, sticky='w')
+
+    lbl_miu = tk.Label(wdw_tectonic_stress, text="Fric. Coefficient")
+    lbl_miu.grid(row=2, column=2, padx=5, pady=5, sticky='w')
+
+    lbl_long_min = tk.Label(wdw_tectonic_stress, text="Long Min")
+    lbl_long_min.grid(row=3, column=2, padx=5, pady=5, sticky='w')
+
+    lbl_long_max = tk.Label(wdw_tectonic_stress, text="Long Max")
+    lbl_long_max.grid(row=4, column=2, padx=5, pady=5, sticky='w')
+
+    lbl_lat_min = tk.Label(wdw_tectonic_stress, text="Lat Min")
+    lbl_lat_min.grid(row=5, column=2, padx=5, pady=5, sticky='w')
+
+    lbl_lat_max = tk.Label(wdw_tectonic_stress, text="Lat Max")
+    lbl_lat_max.grid(row=6, column=2, padx=5, pady=5, sticky='w')
+
+    # Create the input fields for the additional variables
+    inp_E = tk.Entry(wdw_tectonic_stress, textvariable=E)
+    inp_E.grid(row=0, column=3, padx=5, pady=5)
+
+    inp_v = tk.Entry(wdw_tectonic_stress, textvariable=v)
+    inp_v.grid(row=1, column=3, padx=5, pady=5)
+
+    inp_miu = tk.Entry(wdw_tectonic_stress, textvariable=miu)
+    inp_miu.grid(row=2, column=3, padx=5, pady=5)
+
+    inp_long_min = tk.Entry(wdw_tectonic_stress, textvariable=long_min)
+    inp_long_min.grid(row=3, column=3, padx=5, pady=5)
+
+    inp_long_max = tk.Entry(wdw_tectonic_stress, textvariable=long_max)
+    inp_long_max.grid(row=4, column=3, padx=5, pady=5)
+
+    inp_lat_min = tk.Entry(wdw_tectonic_stress, textvariable=lat_min)
+    inp_lat_min.grid(row=5, column=3, padx=5, pady=5)
+
+    inp_lat_max = tk.Entry(wdw_tectonic_stress, textvariable=lat_max)
+    inp_lat_max.grid(row=6, column=3, padx=5, pady=5)
+
+    return
+
+
+def start_loading_screen():
+    global loading_screen
+    # Create a new Toplevel window
+    loading_screen = tk.Toplevel(root)
+
+    # Set the window size and position it in the center of the screen
+    loading_screen.geometry("100x75+{}+{}".format(
+        int(root.winfo_screenwidth()/2 - 100), int(root.winfo_screenheight()/2 - 50)))
+
+    # Create a label to display a message or animation
+    message_label = tk.Label(loading_screen, text="Loading...")
+    message_label.pack(pady=20)
+
+    # Update the window to display the label
+    loading_screen.update()
+    return
+
+
+def end_loading_screen():
+    loading_screen.destroy()
+    return
+
+
+def run_tectonic_stress():
+    start_loading_screen()
+    calculate_tectonic_stress()
+    end_loading_screen()
+    return
+
+
+# root window
+root = tk.Tk()
+root.geometry('785x480')
+# root.resizable(False, False)
+root.title('Menu Demo')
+
+
+# create a menubar
+menubar = tk.Menu(root)
+root.config(menu=menubar)
+
+# create the file_menu
+file_menu = tk.Menu(
+    menubar,
+    tearoff=0
+)
+
+# add menu items to the File menu
+file_menu.add_command(label='Open File', command=open_file)
+file_menu.add_separator()
+
+
+# add Exit menu item
+file_menu.add_separator()
+file_menu.add_command(
+    label='Exit',
+    command=root.destroy
+)
+
+
+menubar.add_cascade(
+    label="File",
+    menu=file_menu,
+    underline=0
+)
+# create the Help menu
+help_menu = tk.Menu(
+    menubar,
+    tearoff=0
+)
+
+help_menu.add_command(label='Calculate Tectonic Stress',
+                      command=window_tectonic_stress)
+help_menu.add_command(label='Calculate Stress Time Function',
+                      command=lambda root=root: window_stress_time_function(root))
+
+# add the Help menu to the menubar
+menubar.add_cascade(
+    label="Function",
+    menu=help_menu,
+    underline=0
+)
+
 root.mainloop()
